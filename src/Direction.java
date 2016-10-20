4e26173ba15d1ad05e12a4302aaafa4a9764989b
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-20 15:43:39 +0100

    Created enum Direction

---
+++ src/Direction.java
@@ -0,0 +1,19 @@
+public enum Direction
+{
+	up	      ( 0, 1 ),
+	upRight   ( 1, 1 ),
+	right     ( 1, 0 ),
+	downRight ( 1,-1 ),
+	down      ( 0,-1 ),
+	downLeft  (-1,-1 ),
+	left      (-1, 0 ),
+	upLeft    (-1, 1 );
+	
+	int x, y;
+	
+	Direction(int x, int y)
+	{ 
+		this.x = x;
+		this.y = y;
+	}
+}
