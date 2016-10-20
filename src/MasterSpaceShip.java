ae4647a46d21388e9a7a08a8e9ddad4b411a91d7
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-20 15:26:28 +0100

    Created MasterSpaceShip class

---
+++ src/MasterSpaceShip.java
@@ -0,0 +1,11 @@
+import java.awt.Point;
+
+public class MasterSpaceShip extends SpaceShip
+{
+	private OperationalMode operationalMode;
+	
+	public MasterSpaceShip(String name, Point position)
+	{
+		super(name, position);
+	}
+}
