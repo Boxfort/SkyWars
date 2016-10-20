3d9e6f37ac1936c4e94f0b86001cddd9b1cdf9bd
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-20 15:22:14 +0100

    Created spaceship class

---
+++ src/SpaceShip.java
@@ -0,0 +1,33 @@
+import java.awt.Point;
+
+public abstract class SpaceShip
+{
+	private String name;
+	private Point position;
+	
+	public SpaceShip(String name, Point position)
+	{
+		this.name = name;
+		this.position = position;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public Point getPosition()
+	{
+		return position;
+	}
+	
+	public void setPosition(Point position)
+	{
+		this.position = position;
+	}
+}
