82616bb4037a62e60ae727e30aca9cb56b82b883
Jack Anderson <40208539@live.napier.ac.uk>
2016-10-20 15:28:04 +0100

    Added default operational mode

--- src/MasterSpaceShip.java
+++ src/MasterSpaceShip.java
@@ -2,7 +2,7 @@ import java.awt.Point;
 
 public class MasterSpaceShip extends SpaceShip
 {
-	private OperationalMode operationalMode;
+	private OperationalMode operationalMode = OperationalMode.defensive;
 	
 	public MasterSpaceShip(String name, Point position)
 	{
